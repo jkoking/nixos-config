4775bff5b27c9a985f46013ac206acd36e5fe8d8
Jacob Osborne <jacobosborne757@gmail.com>
2024-03-25 19:04:39 -0400

    plasma 6 and unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:jkoking/nixos-hardware/add-framework-16";
 
     home-manager = {
