4bdde41863acfea6a2f39bd9e6d064cf450796cc
Jacob Osborne <jacobosborne757@gmail.com>
2024-03-24 21:56:37 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixos-hardware.url = "github:jkoking/nixos-hardware/add-framework-16";
+
+
+     home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+    nixosConfigurations.framework-16 = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/framework-16/configuration.nix
+        ./modules/nixos/kde.nix
+        inputs.home-manager.nixosModules.default
+        nixos-hardware.nixosModules.framework-16-7040-amd
+
+      ];
+    };
+  };
+}
---
+++ modules/nixos/kde.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}:
+{
+    # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    theme = "${pkgs.nordic.sddm}/share/sddm/themes/Nordic-bluish";
+  };
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+  # kde connect
+  programs.kdeconnect.enable = true;
+
+  # add kde pack packags
+  environment.systemPackages = with pkgs.libsForQt5; [
+    akregator
+    dragon
+    kamoso
+    kdenlive
+    kdeplasma-addons
+    kmines
+    knights
+    qtstyleplugin-kvantum
+];
+}
