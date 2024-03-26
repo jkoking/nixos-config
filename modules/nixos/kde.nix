3bcfe322578a4711b79a46c8344dcbdd3d35fc69
Jacob Osborne <jacobosborne757@gmail.com>
2024-03-25 22:23:44 -0400

    Revert "plasma 6 and unstable"
    This reverts commit 4775bff5b27c9a985f46013ac206acd36e5fe8d8.

--- modules/nixos/kde.nix
+++ modules/nixos/kde.nix
@@ -3,15 +3,14 @@
   services.xserver.displayManager.sddm = {
     enable = true;
     theme = "${pkgs.nordic.sddm}/share/sddm/themes/Nordic-bluish";
-    wayland.enable = true;
   };
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # kde connect
   programs.kdeconnect.enable = true;
 
   # add kde pack packags
-  environment.systemPackages = with pkgs.kdePackages; [
+  environment.systemPackages = with pkgs.libsForQt5; [
     akregator
     dragon
     kdenlive
