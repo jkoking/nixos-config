2d24ad24a6dcf4f7173a108bee6222820917e8c6
Jacob Osborne <jacobosborne757@gmail.com>
2024-03-26 10:36:00 -0400

    set back to stable
    This reverts commit 332fb45bd1e81a28e9b4d5c0bc2e6c03df2c4418.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:jkoking/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixos-hardware.url = "github:jkoking/nixos-hardware/add-framework-16";
 
     home-manager = {
