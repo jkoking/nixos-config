e35163643fbc5395b2587813d2aed29f9d29a2af
Jacob Osborne <jacobosborne757@gmail.com>
2024-03-27 10:27:28 -0400

    small to full

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:jkoking/nixos-hardware/add-framework-16";
 
     home-manager = {
