f607a4671f13f5260ac333d7676b0cdea82bd518
Jacob Osborne <jacobosborne757@gmail.com>
2024-04-20 12:39:15 -0400

    Fixed shell implementation

--- hosts/framework-16/configuration.nix
+++ hosts/framework-16/configuration.nix
@@ -174,8 +174,17 @@
 
   # setup shells
   programs.fish.enable = true;
-  users.defaultUserShell = pkgs.fish;
   environment.shells = with pkgs; [ fish bash ];
+  users.defaultUserShell = pkgs.bash;
+  programs.bash = {
+  interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+  };
 
   # Limit journal size
   services.journald.extraConfig = ''
