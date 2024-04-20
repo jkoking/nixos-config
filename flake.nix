424c02b63781e418acc69d405609c7f2332e918c
Jacob Osborne <jacobosborne757@gmail.com>
2024-04-20 16:33:25 -0400

    Switch back to main hardware repo and update 4-20-24

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:jkoking/nixos-hardware/add-framework-16";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
       url = "github:nix-community/home-manager";
