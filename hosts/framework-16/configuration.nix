bbeefe0d46f6d30cee71d335ee8ce1aa1b58417d
Jacob Osborne <jacobosborne757@gmail.com>
2025-07-27 16:46:02 -0400

    added virtiofsd

--- hosts/framework-16/configuration.nix
+++ hosts/framework-16/configuration.nix
@@ -117,6 +117,7 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
+      vhostUserPackages = with pkgs; [ virtiofsd ];
       ovmf = {
         enable = true;
         packages = [
